0a6007a9aa0700b0ca52a8c71cf3176581c8568a
Hubert-Ackuaku-2425400562 <2425400562@live.gctu.edu.gh>
2026-05-31 22:29:23 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,18 +1,22 @@
 # File: initials.py
 # Description: Print out my initials.
-# Assignment Number: 2
+# Assignment Number: 1
 #
 # Name: Hubert Ackuaku
 # STUDENT ID:  2425400562
 # Email: 2425400562@live.gctu.edu.gh
-# Grader: Mr. Augustus Buckman
+# Grader: Buckman
 #
 # On my honor, Hubert Ackuaku, this programming assignment is my own work
 # and I have not provided this code to any other student.
+
 def main():
-print("")
-    print("...DHA")
+    #
     print("")
+    print("")
+    print("...DHA")
+    print("");
+    print("");
     print("...DDDDDDD.............HHHH....HHHH.............AA..........")
     print("...DDD..DDD............HHHH....HHHH............AAAA.........")
     print("...DDD...DDD...........HHHH....HHHH...........AA..AA........")
@@ -23,4 +27,6 @@ print("")
     print("...DDD....DDD..........HHHH....HHHH........AAA......AAA.....")
     print("...DDD...DDD......**...HHHH....HHHH...**...AAA......AAA...**")
     print("...DDDDDDDD.......**...HHHH....HHHH...**...AAA......AAA...**")
+
+
 main()
